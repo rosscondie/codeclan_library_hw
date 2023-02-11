b222419313c4e46acc473c15dbce8ba0f074f8b5
Ross Condie <rosscondie@gmail.com>
2023-02-11 14:28:20 +0100

    added book list to library in library.py

---
+++ models/library.py
@@ -0,0 +1,8 @@
+from models.book import Book
+
+book_1 = Book("Shoe Dog", "Phil Knight", "Biography")
+book_2 = Book("Born to Run", "Christopher McDougall", "Memoir")
+book_3 = Book("Green Lights", "Matthew McConaughey", "Philosophy/Poetry")
+
+books_in_library = [book_1, book_2, book_3]
+
