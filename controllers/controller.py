02f07b08a97af92818e21d69c3bb8130e826fbd5
Ross Condie <rosscondie@gmail.com>
2023-02-11 15:24:04 +0100

    added single_book.html then routed on controller.py to view individual book

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,9 +2,13 @@ from flask import render_template, request, redirect
 
 from app import app
 from models.book import Book
-from models.library import get_book_list
+from models.library import get_book_list, books_list
 
 @app.route("/")
 def index():
     book_list = get_book_list()
     return render_template("index.html", book_list = book_list)
+
+@app.route("/book/<number>")
+def single_book(number):
+    return render_template("single_book.html", book = books_list[int(number)])
